7d90a1618a875c7cd979090c41ec07d1277c3e52
Thomas Young <yk_ecust_2007@163.com>
2019-05-05 16:26:25 +0800

    [feature] 20190505 哲的王

---
+++ leetcode_104/max_depth.py
@@ -0,0 +1,8 @@
+from tree import TreeNode
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
