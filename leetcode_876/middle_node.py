24e51046ba1aef3ccded90eae32a014c7ad5770b
Thomas Young <yk_ecust_2007@163.com>
2019-05-28 23:24:27 +0800

    [feature] 20190528 leetcode 147题 注：代码写的太烂

---
+++ leetcode_876/middle_node.py
@@ -0,0 +1,19 @@
+from tree import ListNode
+
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        list_length = self.get_length(head)
+        start, mid_node = 0, head
+        while start < list_length // 2:
+            start += 1
+            mid_node = mid_node.next
+        return mid_node
+
+    def get_length(self, head):
+        list_length, cur = 0, head
+        while cur:
+            cur = cur.next
+            list_length += 1
+        return list_length
