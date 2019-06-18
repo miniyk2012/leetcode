38a1e96d97ee39f6920c67ef2e658a50daf7f3a6
Thomas Young <yk_ecust_2007@163.com>
2019-06-18 23:17:54 +0800

    recursion red package

--- interview40/my_interview1.py
+++ interview40/my_interview1.py
@@ -4,16 +4,40 @@ from random import randint
 class Solution:
 
     def solve(self, n, m):
+        """傻逼方法"""
         ret = [1] * m
-        n_cents = n * 100 - m
+        n_cents = int(n * 100 - m)
         for _ in range(n_cents):
             idx = randint(0, m - 1)
             ret[idx] += 1
+        ret = list(map(lambda e: e/100, ret))
         return ret
+
+    def solve2(self, n, m):
+        """递归写法"""
+        n = int(n * 100)
+        for ret in self.recursion(n, m, []):
+            yield ret
+
+    def recursion(self, n, m, ret):
+        if (n == 0 and m > 0) or (n > 0 and m == 0):
+            return
+        if n == 0 and m == 0:
+            yield ret
+        for amount in range(1, n + 1):
+            ret.append(amount/100)
+            yield from self.recursion(n-amount, m-1, ret)
+            ret.pop()
 
 
 if __name__ == '__main__':
     s = Solution()
-    ret = s.solve(5, 10)
+    ret = s.solve(0.1, 7)
     print(ret)
-    assert sum(ret) == 5 * 100
+    print('递归写法')
+    i = 0
+    for ret in s.solve2(0.1, 7):
+        print(ret)
+        i += 1
+        if i > 4:
+            break
