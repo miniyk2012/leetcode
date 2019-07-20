63c7e911ff380b6c9eb39ad9e82de6bda12786fc
thomas <yk_ecust_2007@163.com>
2019-07-20 18:22:56 +0800

    [feature] js的set实现
    用object来实现

--- data_structure/5/ht.js
+++ data_structure/5/ht.js
@@ -13,3 +13,16 @@ for (x of a) {
 for (inte in c) {
     console.log(inte, c[inte]);
 }
+
+// node没有基础set类型，用{}伪装成set即可
+console.log("\nnode没有set类型，用{}伪装成set即可")
+let b = [1, "1234x", 1, 2, 3, 4, 5, 5, "1234x"];
+let unique = {};
+for (x of b) {
+    if (!(x in unique)) {
+        unique[x] = x;
+    }
+}
+for (x in unique) {
+    console.log(x);
+}
