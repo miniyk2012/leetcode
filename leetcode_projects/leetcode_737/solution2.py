7738491e00308a32383c4175b439e4b5ba73b86f
Thomas Young <yk_ecust_2007@163.com>
2019-08-07 20:19:56 +0800

    [feature] 重构代码，利用分组优化判断是否连通

--- leetcode_projects/leetcode_737/solution2.py
+++ leetcode_projects/leetcode_737/solution2.py
@@ -3,13 +3,15 @@ from typing import List, Set, Dict
 
 class Solution:
     def __init__(self):
-        self._graph: Dict[str, Set[str]] = {}
+        self._graph: Dict[str, Set[str]] = {}  # 单词: 与它相邻的单词集合
+        self._clusters: Dict[str, int] = {}  # 单词: 组号
 
     def areSentencesSimilarTwo(self, words1: List[str], words2: List[str], pairs: List[List[str]]) -> bool:
         if len(words1) != len(words2):
             return False
 
         self._build_graph(pairs)
+        self._do_cluster()
         return self._judge_similarity(words1, words2)
 
     def _judge_similarity(self, words1, words2):
@@ -18,30 +20,28 @@ class Solution:
                 continue
             if (first not in self._graph) or (second not in self._graph):
                 return False
-            if not self._connected(first, second):
+            if self._clusters[first] != self._clusters[second]:
                 return False
         return True
-
-    def _connected(self, first, second):
-        def _dfs(src, dst):
-            """src是否可达dst"""
-            if src == dst:
-                return True
-            for nxt in self._graph[src]:
-                if nxt in visited:
-                    continue
-                visited.add(nxt)
-                if _dfs(nxt, dst):
-                    return True
-            return False
-
-        visited = set([first])
-        return _dfs(first, second)
 
     def _build_graph(self, pairs: List[List[str]]):
         for first, second in pairs:
             self._graph.setdefault(first, set()).add(second)
             self._graph.setdefault(second, set()).add(first)
+
+    def _do_cluster(self):
+        def _dfs(node: str) -> None:
+            # 将node所在的联通集内的所有单词记录为同一组
+            if node in self._clusters:
+                return
+            self._clusters[node] = _id
+            for nxt in self._graph[node]:
+                _dfs(nxt)
+
+        _id = 0
+        for word in self._graph:
+            _dfs(word)
+            _id += 1
 
 
 def test1():
