0fd7e7d0d9b3deb17dd6e7ac37f7b544ecbb3934
Thomas Young <yk_ecust_2007@163.com>
2019-08-31 08:02:48 +0800

    [feature] 使用工厂方法代替构造函数，使用字符串类名创建对象

--- refractoring/chapter10/10_12.py
+++ refractoring/chapter10/10_12.py
@@ -1,13 +1,11 @@
-"""replace typecode with subclasses
-以子类取代类型码"""
-
+"""replace constructor with factory method
+"""
 import abc
 
 
 class Employee(metaclass=abc.ABCMeta):
     ENGINEER = 0
     SALESMAN = 1
-    MANAGER = 2
 
     __create_key = object()
 
@@ -17,19 +15,22 @@ class Employee(metaclass=abc.ABCMeta):
         assert (create_key == Employee.__create_key), \
             "OnlyCreatable objects must be created using OnlyCreatable.create"
 
-    @property
-    @abc.abstractmethod
-    def occupation_type(self):
-        pass
-
     @classmethod
     def create(cls, _type):
         if _type == Employee.ENGINEER:
             return Engineer(Employee.__create_key)
         elif _type == Employee.SALESMAN:
             return SalesMan(Employee.__create_key)
-        elif _type == Employee.MANAGER:
-            return Manager(Employee.__create_key)
+        else:
+            raise RuntimeError("invalid employee type")
+
+    @classmethod
+    def create_by_name(cls, _type_name):
+        try:
+            import importlib
+            return getattr(importlib.import_module(__name__), _type_name)(Employee.__create_key)
+        except AttributeError:
+            raise TypeError(f"不存在{_type_name}类型的员工")
 
     @abc.abstractmethod
     def describe(self):
@@ -54,32 +55,28 @@ class SalesMan(Employee):
         return Employee.SALESMAN
 
 
-class Manager(Employee):
-    def describe(self):
-        return 'MANAGER'
-
-    @property
-    def occupation_type(self):
-        return Employee.MANAGER
-
-
 def test_employee():
     engineer = Employee.create(Employee.ENGINEER)
     print(engineer.describe())
-    print(engineer.occupation_type)
     salesman = Employee.create(Employee.SALESMAN)
     print(salesman.describe())
-    print(salesman.occupation_type)
-    manager = Employee.create(Employee.MANAGER)
-    print(manager.describe())
-    print(manager.occupation_type)
+    salesman = Employee.create_by_name('SalesMan')
+    print(salesman.describe())
+
     try:
-        Manager('1')
+        SalesMan('1')
     except AssertionError:
         pass
     else:
         raise RuntimeError('不能用构造函数实例化类, 必须用工厂方法实例化类')
 
+    try:
+        Employee.create_by_name('Manager')
+    except TypeError:
+        pass
+    else:
+        raise RuntimeError('不应该进入这里')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     test_employee()
