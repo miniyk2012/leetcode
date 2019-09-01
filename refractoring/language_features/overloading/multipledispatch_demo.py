6f9c01d92077e0466c764208de4d4b6c106e2a63
Thomas Young <yk_ecust_2007@163.com>
2019-09-01 08:47:24 +0800

    [feature] 我的一些重载尝试，本身没啥关注点

---
+++ refractoring/language_features/overloading/multipledispatch_demo.py
@@ -0,0 +1,42 @@
+"""第三方库，不是线程安全的哟"""
+
+from __future__ import annotations
+
+from rbnics_utils_decorators_dispatch import dispatch  # 使用补丁来支持lambda功能 https://github.com/mrocklin/multipledispatch/issues/78
+# Python interpreter fails on @dispatch(F) because F is not fully defined yet
+
+
+class A:
+    def __init__(self, name='a'):
+        self.name = name
+
+    def __str__(self):
+        return super(A, self).__str__() + self.name
+
+    @dispatch(int)
+    def foo(self, a):
+        print(f'int a')
+
+    @dispatch(str)
+    def foo(self, a):
+        print(f'str a')
+
+    @dispatch(str, int)
+    def foo(self, a, b):
+        print(f'str {a} int {b}')
+
+    @dispatch(lambda cls: cls)
+    def foo(self, other):
+        print(f'other {other}')
+
+
+if __name__ == '__main__':
+    a = A()
+    b = A('b')
+    a.foo(1)
+    a.foo("x")
+    a.foo('a', 3)
+    a.foo(b)
+
+    print(a)
+    print(b)
