018931c27fd55023bf12c758962a2d249ab676fb
Thomas Young <yk_ecust_2007@163.com>
2019-09-20 23:41:00 +0800

    [fix] 修改merge_sort, 用yield from 实现

--- data_structure/1/python/merge_sort.py
+++ data_structure/1/python/merge_sort.py
@@ -3,11 +3,12 @@ import heapq
 
 def merge_sort(l, key=None, reverse=None):
     if len(l) <= 1:
-        return l
+        yield from l
+        return
     mid = len(l) // 2
-    return list(heapq.merge(merge_sort(l[:mid], key=key, reverse=reverse),
-                            merge_sort(l[mid:], key=key, reverse=reverse),
-                            key=key, reverse=reverse))
+    yield from heapq.merge(merge_sort(l[:mid], key=key, reverse=reverse),
+                           merge_sort(l[mid:], key=key, reverse=reverse),
+                           key=key, reverse=reverse)
 
 
 if __name__ == '__main__':
@@ -19,7 +20,18 @@ if __name__ == '__main__':
 
     a = [5, 6, 10, 7, -8, 2, 1, 6]
     print(merge_sort(a))
+    print(list(merge_sort(a)))
 
-    print(merge_sort(a, reverse=True))
+    print(list(merge_sort(a, reverse=True)))
 
-    print(merge_sort(a, key=abs))
+    print(list(merge_sort(a, key=abs)))
+
+    g = merge_sort(a, key=abs, reverse=True)
+    container = []
+    while True:
+        try:
+            container.append(next(g))
+        except StopIteration as e:
+            print(container)
+            break
+
