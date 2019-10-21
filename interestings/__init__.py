648f56c34bc35c6807873ff8f23e5fd4e0a1218b
thomas <yk_ecust_2007@163.com>
2019-10-21 15:35:28 +0800

    yield from

--- interestings/__init__.py
+++ interestings/__init__.py
@@ -3,27 +3,61 @@ def my_coroutine():
         received = yield 3
         print('Received', received)
 
-it = my_coroutine()
-val = it.send(None)
-print('Send return', val)
 
-val = it.send(5)
-print('Send return', val)
-
-print()
+# it = my_coroutine()
+# val = it.send(None)
+# print('Send return', val)
+#
+# val = it.send(5)
+# print('Send return', val)
+#
+# print()
 
 def delegated():
     a = yield 1
     print(a)
     b = yield 2
     print(b)
+    return 'c'
+
 
 def composed():
     yield 'A'
     it = delegated()
-    next(it)
+    x = yield next(it)
     while True:
-        r = it.send('x')
+        try:
+            r = it.send(x)
+            yield r
+        except StopIteration as e:
+            print(e.value)
+            break
     yield 'B'
 
+
 it = composed()
+print(next(it))
+while True:
+    try:
+        b = it.send('x')
+        print(b)
+    except StopIteration:
+        break
+print()
+
+
+def new_composed():
+    yield 'A'
+    c = yield from delegated()
+    print(c)
+    yield 'B'
+
+
+it = new_composed()
+print(next(it))
+while True:
+    try:
+        b = it.send('x')
+        print(b)
+    except StopIteration:
+        break
