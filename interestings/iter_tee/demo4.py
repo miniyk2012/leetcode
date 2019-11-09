626b2da35d28d2c7aff885dd0df9a2923a6f4a32
Thomas Young <yk_ecust_2007@163.com>
2019-11-09 23:15:49 +0800

    [feature] 线程安全的tee

--- interestings/iter_tee/demo4.py
+++ interestings/iter_tee/demo4.py
@@ -1,37 +1,24 @@
-import itertools
+import threading
+import time
+from threading import Lock
 
 
 def generator():
-    for i in range(1010000):
+    for i in range(3):
+        time.sleep(1)
         yield i
 
 
 g = generator()
-g_1, g_2 = itertools.tee(g, 2)
-
-import threading
+lock = Lock()
 
 
-def print_sum(x, desc=''):
-    if desc != '':
-        import time
-        time.sleep(2)
-    print(desc, sum(x))
+def do(g):
+    with lock:
+        for x in g:
+            print(x)
 
 
-# print_sum(g_1)
-# print_sum(g_2)
-
-
-for x in g_1, g_2:
-    t = threading.Thread(target=print_sum, args=(x, 'x'), )
-    # t.setDaemon(True)
+for _ in range(2):
+    t = threading.Thread(target=do, args=(g,))
     t.start()
-
-print('-' * 100)
-
-g1 = generator()
-
-g2 = generator()
-for x in g1, g2:
-    threading.Thread(target=print_sum, args=(x,)).start()
---
+++ interestings/iter_tee/demo5.py
@@ -0,0 +1,33 @@
+import threading
+import itertools
+
+
+def generator():
+    for i in range(1000000):
+        yield i
+
+
+g = generator()
+
+class SafeTee:
+    def __init__(self, tee, lock):
+        self.tee_obj = tee
+        self.lock = lock
+
+    def __next__(self):
+        with self.lock:
+            return next(self.tee_obj)
+
+    def __iter__(self):
+        return self
+
+
+def safe_tee(g, n):
+    """线程安全的tee"""
+    lock = threading.Lock()
+    return tuple(SafeTee(tee_obj, lock) for tee_obj in itertools.tee(g, n))
+
+for x in safe_tee(g, 10):
+    t = threading.Thread(target=lambda x: print(sum(x)), args=(x,))
+    t.start()
+
