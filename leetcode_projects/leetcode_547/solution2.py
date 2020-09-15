728c069d2bc58da62171c33e5c5bb3289daf45c6
杨恺 <yangkai02@qutoutiao.net>
2020-09-15 12:00:50 +0800

    [feat]leetcode 547

--- leetcode_projects/leetcode_547/solution2.py
+++ leetcode_projects/leetcode_547/solution2.py
@@ -3,31 +3,26 @@ from typing import List
 
 class Solution:
     def findCircleNum(self, M: List[List[int]]) -> int:
-        """审题有误, 看成矩阵是否连通了, 不过这个做法对错误审题是正确的"""
         self.group_id = 0
         self.M = M
-        group_matrix = [[0 for _ in range(len(M[0]))] for _ in range(len(M))]
+        group = [0] * len(M)
         for i in range(len(M)):
-            for j in range(len(M[0])):
-                self.dfs_group(i, j, group_matrix, True)
+            self.dfs_group(i, group)
         return self.group_id
 
-    def dfs_group(self, i, j, group_matrix, new_visit):
-        if i < 0 or i >= len(self.M):
+    def dfs_group(self, i, group):
+        if i >= len(self.M):
             return
-        if j < 0 or j >= len(self.M[0]):
-            return
-        positions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-        if self.M[i][j] == 0:
-            return
-        if group_matrix[i][j] != 0:
-            return
-        if new_visit:
+        if group[i] == 0:
             self.group_id += 1
-        group_matrix[i][j] = self.group_id
-        for position in positions:
-            m, n = i + position[0], j + position[1]
-            self.dfs_group(m, n, group_matrix, False)
+        group[i] = self.group_id
+        for j in range(0, len(self.M)):
+            if group[j] != 0:
+                continue
+            if self.M[i][j] == 1:
+                group[j] = self.group_id
+                self.dfs_group(j, group)
+
 
 
 if __name__ == '__main__':
