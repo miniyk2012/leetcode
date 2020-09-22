c52f6ea1a0c39aeb5d8dbc883815d325c732dbbb
杨恺 <yangkai02@qutoutiao.net>
2020-09-22 12:56:28 +0800

    prefix 440 part1

--- leetcode_projects/leetcode_440/solution.py
+++ leetcode_projects/leetcode_440/solution.py
@@ -26,9 +26,32 @@ class Solution:
                 heapq.heappush(k_heap, str_i)
         return k_heap[0]
 
+    def findKthNumber1(self, n: int, k: int) -> int:
+        """额, 还是超时"""
+        self.total = 0
+        self.ret = None
+        for i in range(1, 10):
+            self.dfs(0, i, n, k)
+        return self.ret
+
+    def dfs(self, prefix, i, n, k):
+        cur = prefix * 10 + i
+        if cur > n:
+            return
+        self.total += 1
+        if self.total == k:
+            self.ret = cur
+            return
+        for sub in range(0, 10):
+            self.dfs(cur, sub, n, k)
+
+
     def findKthNumber(self, n: int, k: int) -> int:
+        """前缀树有优化空间, 比如可以直接算出前面几颗树有多少节点"""
         pass
+
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.findKthNumber(13, 2))
+    print(s.findKthNumber(13, 10))
+    # print(s.findKthNumber(2, 2))
