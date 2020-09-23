666c9aade246a44b35b6acebbf165001781b3dec
杨恺 <yangkai02@qutoutiao.net>
2020-09-23 09:10:13 +0800

    leetcode 440

--- leetcode_projects/leetcode_440/solution.py
+++ leetcode_projects/leetcode_440/solution.py
@@ -45,13 +45,35 @@ class Solution:
         for sub in range(0, 10):
             self.dfs(cur, sub, n, k)
 
-
     def findKthNumber(self, n: int, k: int) -> int:
-        """前缀树有优化空间, 比如可以直接算出前面几颗树有多少节点"""
-        pass
+        """前缀树有优化空间, 可以直接得出某棵前缀树里面小于等于n的节点数"""
+
+        def get_count(prefix, n):
+            """某棵前缀树里面小于等于n的节点数"""
+            cnt = 0
+            a, b = prefix, prefix + 1
+            while a <= n:
+                cnt += min(b, n + 1) - a
+                a *= 10
+                b *= 10
+            return cnt
+
+        # 逐步扩充前缀树, 找到最终结果
+        prefix = 1
+        p = 1  # 当前的前缀树的排名
+        while p < k:
+            cnt = get_count(prefix, n)
+            if cnt + p <= k:
+                prefix += 1  # 位置往右移动, 排名增加左边的那颗前缀树中所有节点
+                p += cnt
+            else:
+                prefix *= 10
+                p += 1  # 位置往下移动, 排名+1
+        return prefix
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.findKthNumber(13, 10))
-    # print(s.findKthNumber(2, 2))
+    print(s.findKthNumber(13, 2))
+    print(s.findKthNumber(10, 3))
+    print(s.findKthNumber2(10, 3))
