5b3ee5ca8d434a55f422fd6eabb4194f1d42637a
Thomas Young <yk_ecust_2007@163.com>
2020-09-25 08:34:55 +0800

    leetcode617 BFS

--- leetcode_projects/leetcode_617/solution.py
+++ leetcode_projects/leetcode_617/solution.py
@@ -1,6 +1,6 @@
 from leetcode_projects.tree import TreeNode, construct_tree, bfs_visit_tree
 
-
+from collections import deque
 class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
         if t1 is None:
@@ -12,6 +12,44 @@ class Solution:
             t1.left = self.mergeTrees(t1.left, t2.left)
             t1.right = self.mergeTrees(t1.right, t2.right)
         return t1
+
+    def mergeTrees2(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        """BFS"""
+        if t1 is None:
+            return t2
+        elif t2 is None:
+            return t1
+        stack1 = deque([t1])
+        stack2 = deque([t2])
+        head = TreeNode(t1.val + t2.val)
+        stack3 = deque([head])
+        while stack1:
+            node1 = stack1.popleft()
+            node2 = stack2.popleft()
+            node3 = stack3.popleft()
+
+            if node1.left is None:
+                node3.left = node2.left
+            elif node2.left is None:
+                node3.left = node1.left
+            else:
+                stack1.append(node1.left)
+                stack2.append(node2.left)
+                left = TreeNode(node1.left.val + node2.left.val)
+                node3.left = left
+                stack3.append(left)
+
+            if node1.right is None:
+                node3.right = node2.right
+            elif node2.right is None:
+                node3.right = node1.right
+            else:
+                stack1.append(node1.right)
+                stack2.append(node2.right)
+                right = TreeNode(node1.right.val + node2.right.val)
+                node3.right = right
+                stack3.append(right)
+        return head
 
 
 if __name__ == '__main__':
